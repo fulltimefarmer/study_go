adf7d1c8be8be5dd0301d338b025213ded06a026
zhoujun <zhoujun9633@163.com>
2023-04-11 14:39:06 +0800

    Add files via upload

---
+++ forward.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "net/http/httputil"
+    "net/url"
+)
+
+func main() {
+    // 定义反向代理服务器的地址
+    targetUrl, err := url.Parse("https://www.baidu.com/")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // 创建反向代理服务器
+    proxy := httputil.NewSingleHostReverseProxy(targetUrl)
+
+    // 处理客户端请求
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        // 设置Header
+        w.Header().Set("X-Proxy", "go-reverse-proxy")
+
+        // 修改请求的Host和Scheme
+        r.Host = "www.baidu.com"
+        r.URL.Scheme = "https"
+        r.URL.Host = "www.baidu.com"
+
+        // 转发请求
+        proxy.ServeHTTP(w, r)
+    })
+
+    // 启动服务器
+    fmt.Println("Starting server on :9090")
+    if err := http.ListenAndServe(":9090", nil); err != nil {
+        log.Fatal(err)
+    }
+}
